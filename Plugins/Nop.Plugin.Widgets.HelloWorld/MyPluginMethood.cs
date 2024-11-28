f5f18544c92c791a2b665048b95aac72924c0b60
hasanooo <raihanmahmud10112@gmail.com>
2024-11-29 00:26:58 +0600

    Add project files.

---
+++ Plugins/Nop.Plugin.Widgets.HelloWorld/MyPluginMethood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widgets.HelloWorld
+{
+    public class MyPluginMethood :BasePlugin
+    {
+
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+        }
+         public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+
+    }
+}
