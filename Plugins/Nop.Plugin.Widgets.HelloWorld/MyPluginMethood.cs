51ce9864c9d895fc282fa34fbfb5d721f0d62dc2
hasanooo <raihanmahmud10112@gmail.com>
2024-12-03 00:09:00 +0600

    helloword plugin add

--- Plugins/Nop.Plugin.Widgets.HelloWorld/MyPluginMethood.cs
+++ Plugins/Nop.Plugin.Widgets.HelloWorld/MyPluginMethood.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nop.Services.Cms;
 using Nop.Services.Plugins;
 
 namespace Nop.Plugin.Widgets.HelloWorld
 {
-    public class MyPluginMethood :BasePlugin
+    public class MyPluginMethood :BasePlugin, IWidgetPlugin
     {
+        public bool HideInWidgetList => false;
+
+
+        public Type GetWidgetViewComponent(string widgetZone)
+        {
+            return typeof(ExampleWidgetViewComponent);
+        }
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { "home_page_before_categories" });
+        }
 
         public override async Task InstallAsync()
         {
